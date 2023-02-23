a5453dd3f5f5d7bbc0aa7bcef778350e4d94ca80
aleksandarT <aleksandar.trmcic@gmail.com>
2023-02-23 08:51:35 +0100

    first commit fe41

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
