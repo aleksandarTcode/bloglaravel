1c4adc021e9ebcd51d22354c1945334aaea539e4
aleksandarT <aleksandar.trmcic@gmail.com>
2023-02-25 11:09:09 +0100

    fe49

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,11 +22,12 @@ class RegisterController extends Controller
             'password' => 'required|min:7|max:255'
             ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        // log the user in
+        auth()->login($user);
 
 //        session()->flash('success', 'Your account has been created.');
-
         return redirect('/')->with('success', 'Your account has been created.');
-
     }
 }
