9cb943a70042be007fc92ddef469f29ff25e479f
aleksandarT <aleksandar.trmcic@gmail.com>
2023-02-25 09:18:11 +0100

    fe47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,9 +16,16 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
+
 
                 <div class="mb-6">
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -30,8 +37,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -44,8 +56,13 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -60,6 +77,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -69,6 +90,10 @@
                         Submit
                     </button>
                 </div>
+
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
 
             </form>
         </main>
