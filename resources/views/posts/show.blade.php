38203da744598a8078954a7af9b58d5d48f50a96
aleksandarT <aleksandar.trmcic@gmail.com>
2023-02-28 16:48:02 +0100

    fe57

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,35 +50,7 @@
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
 
-                    @auth
-                        <x-panel>
-                            <form method="POST" action="/posts/{{ $post->slug }}/comments">
-                                @csrf
-
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="60" class="rounded-full">
-
-                                    <h2 class="ml-4">Want to participate?</h2>
-                                </header>
-
-                                <div class="mt-6">
-                                <textarea name="body" rows="5"
-                                          class="w-full text-sm focus:outline-none focus:ring" placeholder="Quick, think of something to say!"></textarea>
-                                </div>
-
-                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                                </div>
-                            </form>
-                        </x-panel>
-
-                    @else
-                        <p class="font-semibold">
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
-                        </p>
-                    @endauth
-
-
+                    @include('posts._add-comment-form')
 
 
                     @foreach($post->comments as $comment)
