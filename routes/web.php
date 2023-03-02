4ccda270d103262530c825f6127eaee3e83bf961
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-02 10:48:34 +0100

    fe63

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
 
 
