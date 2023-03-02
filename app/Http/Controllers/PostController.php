55a402629300a79361bd9b1284385d06e4512188
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-02 09:42:34 +0100

    fe62

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,4 +19,9 @@ class PostController extends Controller
             'post' => $post
         ]);
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,6 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+
 
