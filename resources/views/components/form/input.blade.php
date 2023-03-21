ce24fbed7d0b95a3cf718d1ce1d3ed2fecc8504c
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-21 13:22:20 +0100

    fixed input.blade, now showing red insted black warning

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,14 +1,14 @@
-@props(['name','type'=>'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
 
-    <input type="{{ $type }}"
-           class="border border-gray-200 p-2 w-full rounded"
+    <input class="border border-gray-200 p-2 w-full rounded"
            name="{{ $name }}"
            id="{{ $name }}"
-           {{ $attributes(['value'=>old($name)]) }}
+        {{ $attributes(['value' => old($name)]) }}
+    >
 
-    <x-form.error name="{{ $name }}" />
+    <x-form.error name="{{ $name }}"/>
 </x-form.field>
 
