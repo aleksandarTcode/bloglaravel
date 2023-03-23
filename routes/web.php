d050d040e39f814e1ac36d10f9e176a45de80f5f
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-23 12:40:42 +0100

    newsletter updated

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
