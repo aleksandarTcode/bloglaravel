517e1434b749efcb9a3aa738d2e606fe08e31015
aleksandarT <aleksandar.trmcic@gmail.com>
2023-04-10 08:29:35 +0200

    minor fix

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -22,15 +22,6 @@
                                         </div>
 
 
-{{--                                        <div class="flex items-center justify-between space-x-2 mb-10">--}}
-{{--                                            <h1 class="font-bold text-3xl lg:text-4xl">--}}
-{{--                                                {{ $post->title }}--}}
-{{--                                            </h1>--}}
-{{--                                            <x-bookmark-button :post="$post" :bookmarks="$bookmarks"/>--}}
-{{--                                        </div>--}}
-
-
-
 
                                     </td>
 
